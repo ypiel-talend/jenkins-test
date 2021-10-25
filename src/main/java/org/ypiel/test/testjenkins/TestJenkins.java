62ee7f350986a245e35d1d9ec0e427567ea4f620
ypiel <ypiel@talend.com>
2021-10-25 07:08:33 +0200

    Add maven project

---
+++ src/main/java/org/ypiel/test/testjenkins/TestJenkins.java
@@ -0,0 +1,9 @@
+package org.ypiel.test.testjenkins;
+
+public class TestJenkins {
+
+    public final static void main(String[] args){
+        System.out.println("XXXXXXXXXXXXXXXXXXXXx Test Jenkin OK.");
+    }
+
+}
