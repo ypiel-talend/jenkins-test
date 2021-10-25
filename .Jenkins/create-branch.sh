12c525c22c8570dc57fa0c5899a2c1034ab2d439
ypiel <ypiel@talend.com>
2021-10-25 08:08:50 +0200

    A new branch

--- .Jenkins/create-branch.sh
+++ .Jenkins/create-branch.sh
@@ -1,6 +1,7 @@
 #! /bin/bash
 
 echo "create-branch"
+echo "A NEW BRANCH !!!!!!!!!!!"
 branch_version=$(mvn org.apache.maven.plugins:maven-help-plugin:3.2.0:evaluate -Dexpression=project.version -q -DforceStdout)
 release_version=$(echo ${branch_version} | cut -d- -f1)
 branch_name=${BRANCH_NAME:=$(git rev-parse --abbrev-ref HEAD)} # if BRANCH_NAME is not set, retrieve branch name
